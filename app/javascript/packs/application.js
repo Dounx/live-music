19565b4e65a779813be6d304660d63dcce10c24e
Dounx <imdounx@gmail.com>
2020-07-29 15:24:23 +0800

    Fix useless require

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,8 +12,8 @@ require('@rails/activestorage').start();
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-const images = require.context('../images', true);
-const imagePath = (name) => images(name, true);
+// const images = require.context('../images', true);
+// const imagePath = (name) => images(name, true);
 
 import 'styles/application';
 
