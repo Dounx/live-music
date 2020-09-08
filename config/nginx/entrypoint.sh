7800309dda355ed1aae64760ec489bd1f2b051d5
Dounx <imdounx@gmail.com>
2020-09-08 17:07:20 +0800

    Fix bugs

--- config/nginx/entrypoint.sh
+++ config/nginx/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-envsubst "$SITE_DOMAIN" < /usr/src/live-music/config/nginx/templates/default.conf.template > /etc/nginx/sites-enabled/default
+# DON'T USE ""
+# shellcheck disable=SC2016
+envsubst '$SITE_DOMAIN' < /usr/src/live-music/config/nginx/templates/default.conf.template > /etc/nginx/sites-enabled/default
 
 nginx
 
