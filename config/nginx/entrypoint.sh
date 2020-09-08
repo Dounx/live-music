e64de974987c235c8d3f9eed814c6fed65629b72
Dounx <imdounx@gmail.com>
2020-09-08 16:20:35 +0800

    Fix web service

--- config/nginx/entrypoint.sh
+++ config/nginx/entrypoint.sh
@@ -2,6 +2,8 @@
 
 envsubst "$SITE_DOMAIN" < /usr/src/live-music/config/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
+nginx
+
 /usr/src/live-music/bin/install-ssl
 
 exec "$@"
